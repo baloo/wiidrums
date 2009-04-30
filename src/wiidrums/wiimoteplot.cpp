28e1d5dcfff06ef71fd9cb150eff217890b7475b
Arthur Gautier <superbaloo@superbaloo.net>
2009-04-30 10:09:26 +0200

    Ajout d'un ersatz de plot

---
+++ src/wiidrums/wiimoteplot.cpp
@@ -0,0 +1,10 @@
+// vim: ts=4 sw=4 expandtab:
+
+
+#include "wiimoteplot.h"
+
+WiimotePlot::WiimotePlot(wiimote_t * wm):
+    Wiimote(wm)
+{
+
+}
---
+++ src/wiidrums/wiimoteplot.h
@@ -0,0 +1,22 @@
+// vim: ts=4 sw=4 expandtab:
+
+#ifndef DEF_WIIMOTEPLOT
+#define DEF_WIIMOTEPLOT
+
+#include "wiimote.h"
+
+
+class WiimotePlot : public Wiimote
+{
+  public:
+    WiimotePlot(wiimote_t * wm);
+    ~Wiimote();
+
+  protected:
+
+};
+
+
+
+#endif
+
